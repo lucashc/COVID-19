3baeb95c971d155343f42273bda69b5203ba0938
Lucas Crijns <lucascrijns@gmail.com>
2020-09-20 00:35:28 +0200

    Improved performance of make_edge function

--- scripts/experimental/benchmark.R
+++ scripts/experimental/benchmark.R
@@ -2,17 +2,24 @@ source('./scripts/experimental/graph.R')
 library(bench)
 
 n <- 100
-node_data <- generate_node_data(n)
+#node_data <- generate_node_data(n)
 
 
-bench_func <- function(vec){
+bench_func_connect <- function(vec){
   for (i in 1:n) {
     connect(i, vec)
   }
 }
 
+bench_func_make <- function(graph) {
+  for (i in 1:as.integer(n/2)) {
+    for (j in as.integer(n/2):n) {
+      graph <- make_edge(graph, i, j)
+    }
+  }
+}
 
 mark(
-  {vec <- 1:100
-  bench_func(vec)}
+  {graph <- generate_empty_graph(n)
+  bench_func_make(graph)}
 )
