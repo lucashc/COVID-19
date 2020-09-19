b0e2814614c8e8cdab8e9c864d0e35fd5cf77979
Benjamin <benjamin.oudejans@gmail.com>
2020-09-19 16:34:45 +0200

    In functies ingepakt

--- scripts/experimental/graph.R
+++ scripts/experimental/graph.R
@@ -1,63 +1,36 @@
-source("geodata.R")
+source("./scripts/experimental/geodata.R")
 
-n <- 1000
-lambda <- 0.005
-alpha <- 1
-
-
-
-weights <- rep.int(1, n)
-
-
-sample = geo.sample(n)
-x <- sample$x
-y <- sample$y
-node_data <- data.frame(weights, x, y)
-names(node_data) <- c('weight', 'x', 'y')
-
-
-graph = list()
-for (i in 1:n){
-  graph[[i]] <-  vector()
+generate_node_data <- function(n, weights = rep.int(1, n), accuracy = 1000){
+  sample = geo.sample(n, accuracy)
+  x <- sample$x
+  y <- sample$y
+  node_data <- data.frame(weights, x, y)
+  names(node_data) <- c('weight', 'x', 'y')
+  return(node_data)
 }
 
 
+
+generate_empty_graph <- function(n){
+  graph = list()
+  for (i in 1:n){
+    graph[[i]] <-  vector()
+  }
+  return(graph)
+}
 
 
 distance_c <- function(node1, node2){
   x = node_data$x
   y = node_data$y
-  return ((x[node1]-x[node2])**2 + (y[node1]-y[node2])**2)**0.5
+  return((x[node1]-x[node2])**2 + (y[node1]-y[node2])**2)**0.5
 }
 
 
 
-connect <- function(node1, node2){
+connect <- function(node1, node2, alpha = 1, lambda = 1){
   w = node_data$weight
   p <- runif(1, 0, 1)[1]
   prob <- 1 - exp(-lambda*w[node1]*w[node2]/(distance_c(node1, node2))^alpha)
-  
-  if(p < prob) {
-    return(TRUE)
-  }
-  else {
-    return(FALSE)
-  }
+  return(p < prob)
 }
-
-
-
-for (node1 in 1:n){
-  for (node2 in 1:n){
-    if (node1 == node2){
-      next
-    }
-    if (connect(node1, node2)){
-      graph[[node1]] = c(graph[[node1]], node2)
-      graph[[node2]] = c(graph[[node2]], node1)
-    }
-  }
-}
-
-print(graph)
-
