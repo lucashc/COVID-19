aeea21a042ff6caec6f0d4c8e491a020c06fbc98
Lucas Crijns <lucascrijns@gmail.com>
2020-09-23 12:32:05 +0200

    Added initial heatmap support

--- scripts/experimental/plot.R
+++ scripts/experimental/plot.R
@@ -10,3 +10,15 @@ plotSIRJ <- function(history, title="SIRJ-plot", S=TRUE, I=TRUE, R=TRUE, J=TRUE)
   print(fig)
   fig
 }
+
+plotHeatMap<- function(node_data, title="Heat map of infections", from=1) {
+  raw_data <- node_data[node_data$status == 2, c('x', 'y')]
+  geo <- geo.getMask()
+  geo <- t(apply(geo, 2, rev))
+  geo[geo == 0] <- NA
+  geo[geo != NA] <- 0
+  jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
+  d <- ggplot()
+  d <- d + geom_raster(data=melt(geo), mapping=aes(x=Var1, y=Var2, fill=value))
+  d <- d + geom_bin2d(data=raw_data, aes(x, y), binwidth=10) + scale_fill_continuous(type='viridis') 
+}
