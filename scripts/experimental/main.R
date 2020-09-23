10301b0d39d68d1eabf6a6fa70dc092f3a2bbb72
Lucas Crijns <lucascrijns@gmail.com>
2020-09-23 10:56:44 +0200

    Progress bar update, no printing

--- scripts/experimental/main.R
+++ scripts/experimental/main.R
@@ -42,7 +42,7 @@ history <- data.frame(day=0, S=n-initial_infections, I=0, R=0, J=initial_infecti
 
 # 2
 pb <- progress_bar$new(total = n_days, format=" Simulating [:bar] :percent :current/:total I: :I J: :J")
-pb$tick(0, tokens=list(I=0, J=initial_infections))
+pb <- pb$tick(0, tokens=list(I=0, J=initial_infections))
 for (i in 1:n_days) {
   statusI <- node_data$status == 2
   statusR <- node_data$recovery_time <= i
