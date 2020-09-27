a14acf702b58f5f3704870ab15fdd37727dc217a
stholtrop <yadeses@gmail.com>
2020-09-27 13:54:51 +0200

    Wrapped everything in a nice function

--- scripts/experimental/parameter_search.R
+++ scripts/experimental/parameter_search.R
@@ -16,35 +16,33 @@ search <- function(n, sample_size, lambda, alpha) {
     susceptible = (s+1):n
     total_edges = total_edges + sum(connect(node_data, s, susceptible, alpha, lambda))
   }
-  print(total_edges)
   return(total_edges/sample_size)
 }
 
-average_avg_edges <- function(alpha, n, lambda, shots = 5) {
-  x = replicate(shots, search(n, n, lambda, alpha))
+average_avg_edges <- function(alpha, n, lambda, sample_size, shots = 5) {
+  x = replicate(shots, search(n, sample_size, lambda, alpha))
   print(sprintf("variance: %f", var(x)))
   return(mean(x))
 }
 
-alpha = 1.5*0.5
-n = 10000
-step = 1
-lambda = 0
-target = 7.95
-guess = average_avg_edges(alpha, n, step)
-while(guess < target) {
-  step = step * 2
-  guess = average_avg_edges(alpha, n, step)
-  print(sprintf("guess: %.2f", guess))
-}
-while(abs(guess - target) > 0.1) {
-  step = step/2
-  guess = average_avg_edges(alpha, n, lambda + step)
-  if (guess < target) {
-    lambda = lambda + step
-    print(sprintf("lambda: %.2f", lambda))
+find_lambda <- function(n, alpha, sample_size, step=1e-4, shots = 10, target = 7.95) {
+  lambda = 0
+  guess = average_avg_edges(alpha, n, step, sample_size, shots)
+  while(guess < target) {
+    step = step * 2
+    guess = average_avg_edges(alpha, n, step, sample_size, shots)
+    print(sprintf("guess: %.2f", guess))
   }
-  print(sprintf("guess: %.2f", guess))
+  while(abs(guess - target) > 0.1) {
+    step = step/2
+    guess = average_avg_edges(alpha, n, lambda + step, sample_size, shots)
+    if (guess < target) {
+      lambda = lambda + step
+      print(sprintf("lambda: %.2f", lambda))
+    }
+    print(sprintf("guess: %.2f", guess))
+  }
+  
+  print(sprintf("final lambda: %.2f", lambda))
+  return(lambda)
 }
-
-print(sprintf("lambda: %.2f", lambda))
