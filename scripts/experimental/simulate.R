1a7bf98e1f642252acf4d415e62fd644e124f5cf
Sven Holtrop <yadeses@gmail.com>
2020-09-28 02:48:40 +0200

    Super speed up

--- scripts/experimental/simulate.R
+++ scripts/experimental/simulate.R
@@ -29,7 +29,7 @@ simulate <- function(n=1e6, initial_infections=10, n_days=20, infection_prob=0.0
   
   initial_infected = sample(nrow(node_data), initial_infections)
   node_data[initial_infected, "status"] <- 1
-  node_data[initial_infected, "recovery_time"] <- 14
+  node_data[initial_infected, "recovery_time"] <- recoverytime[initial_infected]
   
   history <- data.frame(day=0, S=n-initial_infections, I=0, R=0, J=initial_infections)
   
@@ -40,7 +40,7 @@ simulate <- function(n=1e6, initial_infections=10, n_days=20, infection_prob=0.0
   }
   
   # 2
-  pb <- progress_bar$new(total = n_days, format=" Simulating [:bar] :percent :current/:total I: :I J: :J")
+  pb <- progress_bar$new(total = n_days, format=" Simulating [:bar] :percent :current/:total :elapsed I: :I J: :J")
   pb <- pb$tick(0, tokens=list(I=0, J=initial_infections))
   for (i in 1:n_days) {
     # Make edges to new people if they are not infected. This works parallel on Linux.
@@ -56,17 +56,17 @@ simulate <- function(n=1e6, initial_infections=10, n_days=20, infection_prob=0.0
     infected <- which(statusI)
     
     # Handle recovery
-    recovered <- which(statusI & statusR)
-    node_data[recovered, "status"] <- 3
+    node_data[statusI & statusR, "status"] <- 3
     
     # Infect people
-    for (node in infected) {
+    infneighs = do.call(c, mapply(function(node){
       neighbours <- graph[[node]]
       susneighs <- neighbours[which(node_data[neighbours,"status"] == 0)]
-      infneighs <- susneighs[runif(length(susneighs)) < infection_prob]
-      node_data[infneighs,"status"] <- 1
-      node_data[infneighs,"recovery_time"] <- i + recoverytime[infneighs]
-    }
+      return(susneighs[runif(length(susneighs)) < infection_prob])
+    },infected))
+
+    node_data[infneighs,"status"] <- 1
+    node_data[infneighs,"recovery_time"] <- i + recoverytime[infneighs]
     
     # Log data
     new_n_inf <- sum(node_data$status == 1)
