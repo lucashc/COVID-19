63f15da3c86a62d12fa6369ae5cc4e4fdf3fb253
Lucas Crijns <lucascrijns@gmail.com>
2020-09-28 15:13:35 +0200

    Initial weight idea

--- scripts/experimental/main.R
+++ scripts/experimental/main.R
@@ -1,12 +1,19 @@
 source('scripts/experimental/simulate.R')
 source('./scripts/experimental/plot.R')
-result <- simulate(monitor = TRUE)
+library(EnvStats)
+
+n <- 1e6
+
+weights <- rpareto(n, 3/4, 4)
+
+result <- simulate(monitor = TRUE, weights=weights, lambda=1e-3)
 
 # print(result$history)
-# diag <- graph_diagnostics(result$graph, result$node_data, groups = c(1,2))
-# print(diag)
-# plot(diag)
+diag <- graph_diagnostics(result$graph, result$node_data, groups = c(1,2))
+print(diag)
+plot(diag)
 
 plot(result$diagnostic_history)
+
 plotSIRJ(result, S=FALSE)
 plotHeatMap(result, from = 2)
