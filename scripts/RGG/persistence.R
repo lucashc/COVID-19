e9018ff8830f3ace414aa2f7510dc7f2ae8e4f06
Lucas Crijns <lucascrijns@gmail.com>
2020-09-29 21:38:36 +0200

    Disable force compression

--- scripts/RGG/persistence.R
+++ scripts/RGG/persistence.R
@@ -1,6 +1,6 @@
 save_result <- function(result, prefix="result", suffix=Sys.time()) {
   filename <- paste("./history/", prefix, " ", suffix, ".Rdata", sep="")
-  save(result, file=filename, compress=TRUE)
+  save(result, file=filename)
 }
 
 load_result <- function(name="result") {
