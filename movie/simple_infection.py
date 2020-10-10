b6da93364c8ab90e6644044156f62ad1ad830e55
Benjamin <benjamin.oudejans@gmail.com>
2020-10-10 20:13:31 +0200

    Added folder for movie stuff including the simple infection animation

---
+++ movie/simple_infection.py
@@ -0,0 +1,22 @@
+from manim import *
+from manim.constants import *
+
+
+class Graph(Scene):
+    def construct(self):
+        infected = Circle(color=RED, radius=0.3)
+        susceptible = Circle(color=BLUE, arc_center=2*UP + 2*RIGHT, radius=0.3)
+        newly_infected = Circle(color=RED, arc_center=2*UP + 2*RIGHT, radius=0.3)
+        line = Line(0.22*UP + 0.22*RIGHT, 1.78*UP + 1.78*RIGHT)
+        red_line = Line(0.22*UP + 0.22*RIGHT, 1.78*UP + 1.78*RIGHT, color=RED)
+        self.play(FadeIn(infected))
+        self.play(FadeIn(susceptible))
+        self.wait(0.5)
+        self.play(FadeIn(line))
+        self.wait(0.5)
+        self.play(Transform(line, red_line))
+        self.play(ShowCreation(red_line))
+        self.wait()
+        self.play(Transform(susceptible, newly_infected))
+
+# python -m manim simple_infection.py Graph -p  to run
