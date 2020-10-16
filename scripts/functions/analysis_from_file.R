f71a736a9e8225a136fccef64b8379ebcc28c96c
Lucas Crijns <lucascrijns@gmail.com>
2020-10-16 22:04:10 +0200

    Analysis of save files

--- scripts/functions/analysis_from_file.R
+++ scripts/functions/analysis_from_file.R
@@ -13,3 +13,17 @@ end_day = 125
 
 plotConfCumRKI(histories, start_day, end_day, 80, 0.68)
 plotConfJRKI(histories, start_day, end_day, 80, 0.68)
+
+# Histogram of maxima
+maxima <- lapply(histories, function(h){max(h$J)})
+hist(as.numeric(maxima), 20)
+
+# Smoothed plot
+start_day = 25
+end_day = 125
+total <- data.frame(histories)
+indices <- grep('^J', colnames(total))
+total[,indices] <- total[,indices]*scale
+dat <- as.matrix(total[,grep('^J', colnames(total))])
+Jmean <- apply(dat, 1, mean)
+plot((start_day+3):(end_day-3), moving_average(Jmean, 7), type='l')
