49e97a9830650a343918123ffda93da76ad9185a
Lucas Crijns <lucascrijns@gmail.com>
2020-10-16 17:07:58 +0200

    Analysis from files of runs

---
+++ scripts/functions/analysis_from_file.R
@@ -0,0 +1,14 @@
+source('./scripts/RGG/source.R')
+
+# Get files
+filelist <- list.files(path="./history/", pattern="^largerun")
+
+histories <- lapply(filelist, function(filename) {
+  load_result(tools::file_path_sans_ext(filename))$history
+})
+
+start_day = 25
+end_day = 125
+
+plotConfCumRKI(histories, start_day, end_day, 80)
+plotConfJRKI(histories, start_day, end_day, 80)
