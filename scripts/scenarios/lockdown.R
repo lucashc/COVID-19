ac289d4b5a05f24f7fa9ca69d3cc107bc0780849
Lucas Crijns <lucascrijns@gmail.com>
2020-10-16 15:02:02 +0200

    Added start and end day

--- scripts/scenarios/lockdown.R
+++ scripts/scenarios/lockdown.R
@@ -2,12 +2,15 @@ source("./scripts/RGG/source.R")
 library(EnvStats)
 
 n = 1e6
+start_day = 25
+end_day = 125
+
 
 result <- simulate(n = n, 
                    monitor = TRUE, 
                    weights = rpareto(n, 3/4, 4),
                    lambda = 0.00128125, # Found with binary search
-                   n_days = 100, 
+                   n_days = end_day-start_day, 
                    infection_prob = function(t, s, j) {
                      if (t < 20) {
                        return(0.024)
@@ -32,8 +35,8 @@ result <- simulate(n = n,
 # save_result(result, prefix='Initial spread')
 # Print day before last
 # print(result$history[length(result$history$day)-1,])
-rki.cumsum_plot(100)
-lines(25:125,(result$history$I + result$history$R + result$history$J)*80)
-lines(29:123, moving_average((result$history$I + result$history$R + result$history$J)*80, 7))
+rki.cumsum_plot(end_day)
+lines(start_day:end_day,(result$history$I + result$history$R + result$history$J)*80)
+lines((start_day+3):(end_day-3), moving_average((result$history$I + result$history$R + result$history$J)*80, 7))
 IDR = rki.getIDR(rki.load())
-print((result$history$I + result$history$R + result$history$J)*80 - cumsum(IDR$infections)[25:125])
+print((result$history$I + result$history$R + result$history$J)*80 - cumsum(IDR$infections)[start_day:end_day])
