b0e85eb78f0c0499d5f931abad43588752104702
Sven Holtrop <yadeses@gmail.com>
2020-10-16 14:58:05 +0200

    Moving average plot

--- scripts/scenarios/lockdown.R
+++ scripts/scenarios/lockdown.R
@@ -10,9 +10,9 @@ result <- simulate(n = n,
                    n_days = 100, 
                    infection_prob = function(t, s, j) {
                      if (t < 20) {
-                       return(0.0245)
+                       return(0.024)
                      } else {
-                       return(0.01)
+                       return(0.006)
                      }
                    },
                    lpois = 14,
@@ -32,7 +32,8 @@ result <- simulate(n = n,
 # save_result(result, prefix='Initial spread')
 # Print day before last
 # print(result$history[length(result$history$day)-1,])
-rki.cumsum_plot()
-lines(25:45,(result$history$I + result$history$R + result$history$J)*80)
+rki.cumsum_plot(100)
+lines(25:125,(result$history$I + result$history$R + result$history$J)*80)
+lines(29:123, moving_average((result$history$I + result$history$R + result$history$J)*80, 7))
 IDR = rki.getIDR(rki.load())
-print((result$history$I + result$history$R + result$history$J)*80 - cumsum(IDR$infections)[25:45])
+print((result$history$I + result$history$R + result$history$J)*80 - cumsum(IDR$infections)[25:125])
